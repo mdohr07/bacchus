24687bdb80841a729b2d25991a7f8fb71c5aaab8
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-11-28 11:17:45 +0100

    project setup

---
+++ src/main/java/mblip/bacchus/BacchusApplication.java
@@ -0,0 +1,13 @@
+package mblip.bacchus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacchusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacchusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mblip/bacchus/BacchusApplicationTests.java
@@ -0,0 +1,13 @@
+package mblip.bacchus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacchusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
